17d477c99336502190948861b7c22233b9491b6a
Kevin <magavakevin10@gmail.com>
2022-08-03 21:27:46 -0400

    commit tree 455931194922698fab69d2e5f797318ba7d91e41

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string.at(0).toUpperCase() + string.slice(1)
+  };
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  };
+
+  static titleize(string) {
+    let noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let separateString = string.split(" ")
+    let afterCap = [this.capitalize(separateString[0])]
+    for (let i = 1; i < separateString.length; i++) {
+      if (noCap.includes(separateString[i])) {
+        afterCap.push(separateString[i])
+      } else {
+        afterCap.push(this.capitalize(separateString[i]))
+      };
+    };
+    return afterCap.join(" ")
+  };
+
+};
